9bff87c7ec6f61162890836dd0f0d7e040eaed80
dongxiuling <dxl_web@163.com>
2019-03-24 15:52:05 +0800

    js

--- 移动端/练习/index.js
+++ 移动端/练习/index.js
@@ -7,5 +7,26 @@ $(document).on('swipeUp',function(){
     // 找到下一张图 从下边滑出来   "page-2 moveFromBottom"(iNow+1)
     $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom"  );
 
+    // 0.6s后 设置图片动画显示 
+    setTimeout(function(){
+        $('.page-'+(iNow+1)).children().removeClass('hide');
+        // 滑出的中的小图片 隐藏
+        $('.page-'+iNow).children().addClass('hide');
+        iNow++;
+    }, 600);
+});
+
+$(document).on('swipeDown',function(){
+    // 当前图片 滑下 
+    $('.page-'+iNow).attr('class','page-'+iNow+" moveToBottom");
+    // 上一张图 滑下来 
+    $('.page-'+(iNow-1)).attr('class','page-'+(iNow-1)+" moveFromTop");
+
+    setTimeout(function(){
+        $('.page-'+(iNow-1)).children().removeClass('hide');
+        // 下滑的时候 让出去的小图都隐藏
+        $('.page-'+iNow).children().addClass('hide');
+        iNow--;
+    }, 600);
 
 });
