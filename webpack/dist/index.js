4dca514266f3447be6efa22da4bc21b4284fc7e7
dongxiuling <dxl_web@163.com>
2019-04-10 14:05:28 +0800

    css

--- webpack/dist/index.js
+++ webpack/dist/index.js
@@ -86,14 +86,26 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/index.css":
+/*!***********************!*\
+  !*** ./src/index.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./src/index.css?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("document.body.innerHTML = \"111111111\";\n\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./src/index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\nconsole.log('dfghjkl');\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     mode:'development',
     entry:{
@@ -9,10 +10,38 @@ module.exports = {
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
     },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // use:['style-loader','css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test:/\.(jpg|png|gif)$/,
+                use:[{
+                    loader:'url-loader',
+                    options:{
+                        limit:500,
+                        outputPath:'images/'
+                    }
+                }]
+            }
+        ]
+    },
     plugins:[
         new HtmlPlugin({
-            template:'./src/index.html'
-        })
+            filename:'xx.html',
+            minify:{
+                removeAttributeQuotes:true
+            },
+            template:'./src/index.html',
+            hash:true
+        }),
+        new ExtractTextPlugin('index.css')
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
