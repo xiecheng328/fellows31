c4f79e9461ca3fbcc3ebcfbe2b094ec3f732e7ca
dongxiuling <dxl_web@163.com>
2019-05-08 16:37:22 +0800

    musiclist

--- vueapp/src/views/music.vue
+++ vueapp/src/views/music.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
-音乐
+        <img :src="obj.bg" v-for="(obj,index) in musicList" :key="index"/>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        
+        data(){
+            return {
+                musicList:[]
+            }
+        },
+        created() {
+            axios.get('/data/musiclist.json')
+            .then((result)=>{
+                this.musicList = result.data.albums;
+            })
+        },
     }
 </script>
 
 <style scoped>
+    img{
+       width:50%; 
+    }
 
 </style>
