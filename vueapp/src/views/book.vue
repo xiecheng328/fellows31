9db2faaafb38abb7b2e02c5c686492ab6bafbb80
dongxiuling <dxl_web@163.com>
2019-05-11 10:26:40 +0800

    vue-swipe

--- vueapp/src/views/book.vue
+++ vueapp/src/views/book.vue
@@ -1,15 +1,47 @@
 <template>
     <div>
-图书
+        <swipe class="my-swipe">
+            <swipe-item class="slide1"></swipe-item>
+            <swipe-item class="slide2"></swipe-item>
+            <swipe-item class="slide3"></swipe-item>
+        </swipe>
     </div>
 </template>
-
 <script>
+    require('vue-swipe/dist/vue-swipe.css');
+    import { Swipe, SwipeItem } from 'vue-swipe';
     export default {
-        
+        components: {
+            Swipe,
+            SwipeItem
+        }
     }
 </script>
 
 <style scoped>
+.my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
+}
 
+.slide1 {
+  background-color: green;
+  color: #fff;
+
+}
+
+
+.slide2 {
+  background-color: #ffd705;
+  color: #000;
+
+}
+
+.slide3 {
+  background-color: #ff2d4b;
+  color: #fff;
+
+}
 </style>
