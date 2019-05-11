5b56aa53ea4eb043dd3000011ecca1d1ca626e1f
dongxiuling <dxl_web@163.com>
2019-05-11 14:02:58 +0800

    vue

--- vueapp/src/components/Slide.vue
+++ vueapp/src/components/Slide.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <transition-group name="." tag="ul" class="slide-box">
+        <transition-group name="fade" tag="ul" class="slide-box">
             <li class="slide-item" v-for="(img,index) in imgs" :key="index" v-show="index == idx ">
                 <img :src="img"/>
             </li>
         </transition-group>
     </div>
 </template>
-
 <script>
-
 // [
 //                     "https://img3.doubanio.com/lpic/s24468373.jpg",
 //                     "https://img3.doubanio.com/lpic/s27102925.jpg",
@@ -21,13 +19,24 @@
             return {
                 idx:0
             }
-        }
+        },
+        created() {
+            setInterval(() => {
+                this.idx++;
+                if(this.idx == this.imgs.length ){
+                    this.idx = 0;
+                }
+            }, 1000);
+
+        },
     }
 </script>
 
 <style scoped>
     .slide-box{
         position: relative;
+        overflow: hidden;
+        height:4rem;
     }
     .slide-item{
         position: absolute;
