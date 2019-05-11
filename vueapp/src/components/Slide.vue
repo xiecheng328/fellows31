5ff7f71086da4e83688eb0b12ea556c7163ffeb4
dongxiuling <dxl_web@163.com>
2019-05-11 11:15:40 +0800

    轮播图

---
+++ vueapp/src/components/Slide.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <transition-group name="." tag="ul" class="slide-box">
+            <li class="slide-item" v-for="(img,index) in imgs" :key="index" v-show="index == idx ">
+                <img :src="img"/>
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+
+// [
+//                     "https://img3.doubanio.com/lpic/s24468373.jpg",
+//                     "https://img3.doubanio.com/lpic/s27102925.jpg",
+//                     "https://img3.doubanio.com/lpic/s6989253.jpg"
+//                 ],
+    export default {
+        props:['imgs'],
+        data(){
+            return {
+                idx:0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .slide-box{
+        position: relative;
+    }
+    .slide-item{
+        position: absolute;
+        left:0px;
+        top:0px;
+        width:100%;
+        height:4rem;
+        /* display: none; */
+    }
+    /* .slide-item:first-child{
+        z-index:2;
+    } */
+    .slide-item:first-child{
+        display: block;
+    }
+
+    .slide-item img{
+        width:100%  ;
+        height:100%;
+    }
+
+    /* 过度的动画  */
+    .fade-enter{
+        transform: translateX(-100%);
+    }
+    .fade-enter-active{
+        transition:transform 1s;
+    }
+    .fade-enter-to{
+        transform: translateX(0);
+    }
+    .fade-leave{
+        transform: translateX(0);
+    }
+    .fade-leave-active{
+        transition:transform 1s;
+    }
+    .fade-leave-to{
+        transform: translateX(100%);
+    }
+    
+</style>
--- vueapp/src/views/book.vue
+++ vueapp/src/views/book.vue
@@ -1,25 +1,44 @@
 <template>
     <div>
-        <swipe class="my-swipe">
+        <!-- <swipe class="my-swipe">
             <swipe-item class="slide1"></swipe-item>
             <swipe-item class="slide2"></swipe-item>
             <swipe-item class="slide3"></swipe-item>
-        </swipe>
+        </swipe> -->
+        <slide :imgs="imgs" ></slide>
     </div>
 </template>
 <script>
-    require('vue-swipe/dist/vue-swipe.css');
-    import { Swipe, SwipeItem } from 'vue-swipe';
+    // import 'vue-swipe/dist/vue-swipe.css';
+    // import { Swipe, SwipeItem } from 'vue-swipe';
+    // export default {
+    //     components: {
+    //         Swipe,
+    //         SwipeItem
+    //     }
+    // }
+
+    // 自定义的轮播图组件 
+
+    import Slide from '@/components/Slide.vue';
     export default {
+        data(){
+            return {
+                imgs:[
+                    "https://img3.doubanio.com/lpic/s24468373.jpg",
+                    "https://img3.doubanio.com/lpic/s27102925.jpg",
+                    "https://img3.doubanio.com/lpic/s6989253.jpg"
+                ]
+            }
+        },
         components: {
-            Swipe,
-            SwipeItem
+            Slide
         }
     }
 </script>
 
 <style scoped>
-.my-swipe {
+/* .my-swipe {
   height: 200px;
   color: #fff;
   font-size: 30px;
@@ -43,5 +62,5 @@
   background-color: #ff2d4b;
   color: #fff;
 
-}
+} */
 </style>
