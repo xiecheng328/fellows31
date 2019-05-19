16cc03304e0c5cc26a4a1ad4412ee89542acc03a
dongxiuling <dxl_web@163.com>
2019-05-19 11:43:13 +0800

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
 import A from './components/A'
 import B from './components/B'
+import Home from './components/Home'
 function App() {
   return (
     <div className="App">
         <div>HAHA</div>
         <Router>
+            <Link to="/">首页面</Link>
+            ----
             <Link to="/a">A页面</Link>
             ----
             <Link to="/b">B页面</Link>
+            {/* <Route exact path="/" component={Home}></Route>
             <Route path="/a" component={A}></Route>
-            <Route path="/b" component={B}></Route>
+            <Route path="/b" component={B}></Route> */}
+            <Switch>
+              <Route path="/a" component={A}></Route>
+              <Route path="/b" component={B}></Route>
+              <Route path="/" component={Home}></Route>
+            </Switch>
         </Router>
     </div>
   );
