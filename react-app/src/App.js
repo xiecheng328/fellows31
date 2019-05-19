a690096a0f71a82c11d6229e38dbf6f86255d437
dongxiuling <dxl_web@163.com>
2019-05-19 14:11:54 +0800

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ import A from './components/A'
 import B from './components/B'
 import C from './components/C'
 import Home from './components/Home'
+import Children from './components/Children';
 function App() {
   return (
     <div className="App">
@@ -21,15 +22,37 @@ function App() {
             <Link to="/b">B页面</Link>
             ----
             <Link to="/c/666">C页面</Link>
+            ----
+            <Link to="/d">D页面</Link>
+            <Link to="/children">children</Link>
             {/* <Route exact path="/" component={Home}></Route>
             <Route path="/a" component={A}></Route>
             <Route path="/b" component={B}></Route> */}
-            <Switch>
+            {/* <Switch>
               <Route path="/a/:id" component={A}></Route>
               <Route path="/b" component={B}></Route>
               <Route path="/c/:num" component={C}></Route>
+              <Route path="/d" render={()=>{
+                return <h1>render dddd</h1>
+              }}></Route>
               <Route path="/" component={Home}></Route>
-            </Switch>
+            </Switch> */}
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/a/:id" component={A}></Route>
+              <Route exact path="/b" component={B}></Route>
+              <Route exact path="/c/:num" component={C}></Route>
+              <Route exact path="/d" render={()=>{
+                return <h1>render dddd</h1>
+              }}></Route>
+              <Route path="/children" children={(props)=>{
+                 let str = props.match?'1111':'0000';
+                 console.log(str)
+                 return <Children str={str}></Children>
+              }}></Route>
+
+
+
+
         </Router>
     </div>
   );
---
+++ react-app/src/components/Children.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Children extends Component {
+  render() {
+    return (
+      <div>
+         {this.props.str}
+      </div>
+    )
+  }
+}
