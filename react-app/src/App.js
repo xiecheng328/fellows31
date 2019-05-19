d10d852ddaa6e1815012e53bc6365f0de4f8463f
dongxiuling <dxl_web@163.com>
2019-05-19 11:28:02 +0800

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
+import A from './components/A'
+import B from './components/B'
 function App() {
   return (
     <div className="App">
-      haha
+        <div>HAHA</div>
+        <Router>
+            <Link to="/a">A页面</Link>
+            ----
+            <Link to="/b">B页面</Link>
+            <Route path="/a" component={A}></Route>
+            <Route path="/b" component={B}></Route>
+        </Router>
     </div>
   );
 }
