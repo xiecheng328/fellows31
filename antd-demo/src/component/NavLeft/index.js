e624de576850f0d800411b4e5c63afb20c38f37c
dongxiuling <dxl_web@163.com>
2019-05-25 15:03:14 +0800

    react

--- antd-demo/src/component/NavLeft/index.js
+++ antd-demo/src/component/NavLeft/index.js
@@ -4,34 +4,37 @@ import menuList from './../../config/menuconfig'
 const {SubMenu} = Menu;
 
 export default class NavLeft extends Component {
-    createMenu = (menuList) => {
-        return  menuList.map((elem)=>{
+    creteMenu = (menuList) =>{
+        return menuList.map((elem)=>{
             if(elem.children){
                 return (
-                    <SubMenu title={elem.title} key={elem.path}>
-                        {/* 递归调用  循环生成子菜单  */}
-                        {this.createMenu(elem.children)}
+                    <SubMenu title={<span><Icon type={elem.icon}></Icon>{elem.title}</span>}>
+                        {this.creteMenu(elem.children)}
                     </SubMenu>
                 )
             }
-            return (<Menu.Item key={elem.path}><Icon type={elem.icon}></Icon>
-                    {elem.title}
-                    </Menu.Item>);
+            return (<Menu.Item><Icon type={elem.icon}></Icon>{elem.title}</Menu.Item>)
         })
     }
-    componentWillMount = () => {
-        // 取到map循环出来的数组
-        let list = this.createMenu(menuList);
+    componentWillMount(){
+        let list = this.creteMenu(menuList);
         this.setState({
             list
-        });
+        })
     }
-    
     render() {
         return (
         <div>
             <Menu theme="dark" mode="inline">
-               {this.state.list}
+                {this.state.list}
+                {/* <Menu.Item>菜单项</Menu.Item>
+                <Menu.Item>菜单项</Menu.Item>
+                <Menu.Item>菜单项</Menu.Item>
+                <SubMenu title="子菜单">
+                    <Menu.Item>子菜单项</Menu.Item>
+                    <Menu.Item>子菜单项</Menu.Item>
+                    <Menu.Item>子菜单项</Menu.Item>
+                </SubMenu> */}
             </Menu>
         </div>
         )
